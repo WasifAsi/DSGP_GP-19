c05b7015eefed5f24dc99680dcc9ed5dc96e0e42
Wasif Asi <wasifasi01@gmail.com>
2025-04-02 13:29:15 +0530

    uplaod page loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,27 @@
-"use client"
+"use client";
 
-import { Routes, Route } from "react-router-dom"
-import { AnimatePresence } from "framer-motion"
-import Layout from "./components/Layout"
-import Index from "./pages/Index"
-import UploadPage from "./pages/UploadPage"
-import AboutPage from "./pages/AboutPage"
-import NotFound from "./pages/NotFound"
-import ModelInsightsPage from "./pages/ModelInsightsPage"
+import { Routes, Route, useLocation } from "react-router-dom";
+import Layout from "./components/Layout";
+import Index from "./pages/Index";
+import UploadPage from "./pages/UploadPage";
+import AboutPage from "./pages/AboutPage";
+import NotFound from "./pages/NotFound";
+import ModelInsightsPage from "./pages/ModelInsightsPage";
 
 function App() {
+  const location = useLocation();
+
   return (
-    <AnimatePresence mode="wait">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Index />} />
-          <Route path="/upload" element={<UploadPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/model-insights" element={<ModelInsightsPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </AnimatePresence>
-  )
+    <Routes location={location}>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Index />} />
+        <Route path="upload" element={<UploadPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="model-insights" element={<ModelInsightsPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
